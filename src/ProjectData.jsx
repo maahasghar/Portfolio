71cf3078651d965a0224a65b43589b6e0f0678dc
maahasghar <49345824+maahasghar@users.noreply.github.com>
2020-09-02 21:00:32 -0400

    Update ProjectData.jsx

--- src/ProjectData.jsx
+++ src/ProjectData.jsx
@@ -12,6 +12,13 @@ const ProjectData = [
       "Retro Maze is a two-player game where Donkey must find the exit before Shrek catches him to reach the next level. This game was built as a part of a course project using C/C++ and raspberry pi.",
     url: "https://github.com/maahasghar/Retro_Maze",
   },
+  
+  {
+    title: "Porfolio Website",
+    text:
+      "This portfolio website is created to introduce myself, and also showcase my skills and some of my past projects. It was created using ReactJs, npm, CSS,  and JavaScript.",
+    url: "https://github.com/maahasghar/portfolio_site",
+  },
 ];
 
 export default ProjectData;
